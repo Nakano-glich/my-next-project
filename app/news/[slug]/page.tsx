a5d1a5fa83e01ae503b729032c667bd030e36c88
tenta_nakano <tenta_nakano@itp.co.jp>
2025-01-06 14:41:33 +0900

    7章まで完了

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -8,14 +8,19 @@ type Props = {
     params: {
         slug: string;
     };
+    searchParams: {
+        dk?: string;
+    }
 };
 
-export default async function Page({ params }: Props) {
-    const data = await getNewsDetail(params.slug).catch(notFound);
+export default async function Page({ params, searchParams }: Props) {
+    const data = await getNewsDetail(params.slug, {
+        draftKey: searchParams.dk,
+    }).catch(notFound);
 
     return (
         <>
-            <Article data={data}/>
+            <Article data={data} />
             <div className={styles.footer}>
                 <ButtonLink href="/news">ニュース一覧へ</ButtonLink>
             </div>
